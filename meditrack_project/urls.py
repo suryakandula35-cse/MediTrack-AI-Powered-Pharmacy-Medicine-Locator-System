764f14bc1b62c6e750dc8455784dfe3064ab006d
KANDULA DURGA SURYA SAI <suryakandula35@gmail.com>
2026-05-16 16:25:57 +0530

    Added meditrack_project files

---
+++ meditrack_project/urls.py
@@ -0,0 +1,10 @@
+"""
+URL configuration for meditrack_project project.
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+]
